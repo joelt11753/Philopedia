ace651bdf2ba100668821ab7b14184f60e6a89c4
joelt11753 <joel_tennant@yahoo.com>
2017-08-12 20:33:47 -0700

    User roles added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,31 @@
 require 'random_data'
 
-10.times do
-	Wiki.create!(
-		title: RandomData.random_sentence,
-		body:  RandomData.random_paragraph
+5.times do
+	User.create!(
+		email:    RandomData.random_email,
+		password: RandomData.random_sentence,
+		role:     'standard'
 	)
 end
+
+users = User.all
+
+10.times do
+	Wiki.create!(
+		title:   RandomData.random_sentence,
+		body:    RandomData.random_paragraph,
+		private: false,
+		user:    users.sample
+	)
+end
+
+User.create!(
+	email:    'joel_tennant@yahoo.com',
+	password: 'testing',
+	role:     'admin'
+)
 
 puts "Seeding finishd:"
 
 puts "#{Wiki.count} wiki's created!"
+puts "#{User.count} users created!"
