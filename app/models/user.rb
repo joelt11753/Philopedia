5c39acc8da594b0b5535018e0b4aecd0b0a3a907
joelt11753 <joel_tennant@yahoo.com>
2017-08-18 22:17:10 -0700

    Design Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ActiveRecord::Base
 	after_initialize { self.role ||= :standard }
 
 	enum role: [:standard, :admin, :premium]
+
+	def avatar_url(size)
+		gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+		"http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+	end
 end
